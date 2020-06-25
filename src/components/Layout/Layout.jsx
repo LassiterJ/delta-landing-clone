f43c4592a9f1cace2278f9bf27f04c09c90cb4d8
Jonathan Lassiter <pikapp336@me.com>
2020-06-25 16:18:45 -0400

    Added Theme component for global styles.

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Theme from '../Theme/Theme';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
 
 const Layout = ({ children }) => {
   return (
-    <>
+    <Theme>
       <Header />
       {children}
       <Footer />
-    </>
+    </Theme>
   );
 };
 
